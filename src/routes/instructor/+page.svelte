adc9659b55c287a8d37869aa577cda1160f903e6
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-06-25 15:37:40 +0800

    feat: declare dragEvent handlers

--- src/routes/instructor/+page.svelte
+++ src/routes/instructor/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { assert } from '$lib/assert';
     import type { PageData } from './$types';
     
     export let data: PageData;
@@ -6,15 +7,31 @@
     type Student = {name: string, id: string, email: string}
 
     // these draftee lists are expected to be provided from the page data on load
-    let unselected_draftees: Student[] = [
+    let unselectedDraftees: Student[] = [
         {name: "Victor Edwin Reyes", id: "2021-01588", email: "vereyes2@up.edu.ph"},
         {name: "Sebastian Luis Ortiz", id: "2020-XXXXX", email: "slortiz@up.edu.ph"},
         {name: "Angelica Raborar", id: "2020-XXXXX", email: "araborar@up.edu.ph"},
     ]
-
-    let selected_draftees: Student[] = [
+    
+    let selectedDraftees: Student[] = [
 
     ]
+
+    // these functions are drag event handlers, all event modifiers should be in on directives
+
+    function handleDragStart(e: DragEvent) {
+        assert(e.dataTransfer)
+        e.dataTransfer.dropEffect = "move"
+    }
+
+    function handleDragEnd(e: DragEvent) {
+
+    }
+
+    // handles a drag and drop into an element with an associated drafteesList (must be an array of students)
+    function handleDragDropIntoList(e: DragEvent, drafteesList: Student[]) {
+
+    }
 
 </script>
 
@@ -23,7 +40,7 @@
     <div class = "grid grid-cols-5 w-auto my-6">
         <div class="col-span-2 border-primary-900">
             Unselected Draftees
-            {#each unselected_draftees as draftee (draftee.id)}
+            {#each unselectedDraftees as draftee (draftee.id)}
                 <div class="bg-red-300 m-2 p-2 w-60">
                     <p>{draftee.name}</p>
                     <p>{draftee.id}</p>
@@ -36,7 +53,7 @@
         </div>
         <div class="col-span-2 border-error-500">
             Selected Draftees
-            {#each selected_draftees as draftee (draftee.id)}
+            {#each selectedDraftees as draftee (draftee.id)}
                 <div class="bg-green-300 m-2 p-2 w-60">
                     <p>{draftee.name}</p>
                     <p>{draftee.id}</p>
