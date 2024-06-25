9a605fa0d5ad8751de5730369bd77ed22b15b316
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-06-25 15:17:07 +0800

    fix: separate draftee lists

--- src/routes/instructor/+page.svelte
+++ src/routes/instructor/+page.svelte
@@ -5,18 +5,30 @@
 
     type Student = {name: string, id: string, email: string}
 
-    let draftees: Student[] = [
+    // these draftee lists are expected to be provided from the page data on load
+    let unselected_draftees: Student[] = [
         {name: "Victor Edwin Reyes", id: "2021-01588", email: "vereyes2@up.edu.ph"},
         {name: "Sebastian Luis Ortiz", id: "2020-XXXXX", email: "slortiz@up.edu.ph"},
         {name: "Angelica Raborar", id: "2020-XXXXX", email: "araborar@up.edu.ph"},
     ]
+
+    let selected_draftees: Student[] = {
+
+    }
+
 </script>
 
 <div class="w-auto m-10">
     <h3 class="h3">Drag and Drop desired draftees for this draft round</h3>
     <div class = "grid grid-cols-5 w-auto my-6">
-        <div class="col-span-2 border-primary-900">Div 1</div>
-        <div class="col-span-1 border-secondary-900">Div 2</div>
-        <div class="col-span-2 border-error-500">Div 3</div>
+        <div class="col-span-2 border-primary-900">
+            Unselected Draftees
+        </div>
+        <div class="col-span-1 border-secondary-900">
+            Controls
+        </div>
+        <div class="col-span-2 border-error-500">
+            Selected Draftees
+        </div>
     </div>
 </div>
