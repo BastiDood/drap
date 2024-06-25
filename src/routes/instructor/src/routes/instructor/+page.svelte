a6614a3b657872d79360158d789bf1f3a90e6db9
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-06-25 16:35:59 +0800

    feat: add dummy data

--- src/routes/instructor/src/routes/instructor/+page.svelte
+++ src/routes/instructor/src/routes/instructor/+page.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+    import type { PageData } from './$types'
     
-    export let data: PageData;
+    export let data: PageData
+
+    type StudentStatus = "selected" | "unselected"
+    type Student = {name: string, id: string, email: string, status: StudentStatus}
+
+    let draftees: Student[] = [
+        {name: "Victor Edwin Reyes", id: "2021-01588", email: "vereyes2@up.edu.ph", status: "unselected"},
+        {name: "Sebastian Luis Ortiz", id: "2020-XXXXX", email: "slortiz@up.edu.ph", status: "unselected"},
+        {name: "Angelica Raborar", id: "2020-XXXXX", email: "araborar@up.edu.ph", status: "unselected"},
+    ]
 </script>
 
 <div class="w-auto m-10">
-    <h3 class="h3">Drag and Drop desired draftees for this draft round</h3>
+    <h3 class="h3">Select desired draftees for this draft round</h3>
     <div class = "grid grid-cols-5 w-auto my-6">
         <div class="col-span-2 border-primary-900-50-token">
             Div 1
