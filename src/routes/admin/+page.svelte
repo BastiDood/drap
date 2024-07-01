8f9def5a304d8777886d9c7062511a733c4d4b19
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-01 13:54:38 +0800

    feat: set up admin page

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+
+    let availableLabs = [
+        { id: 0, name: 'Networks and Distributed Systems Laboratory' },
+        { id: 1, name: 'Computer Vision & Machine Intelligence Laboratory' },
+        { id: 2, name: 'Service Science & Software Engineering Laboratory' },
+        { id: 3, name: 'Algorithms & Complexity Laboratory' },
+        { id: 4, name: 'Web Science Laboratory' },
+        { id: 5, name: 'Computer Security Laboratory' },
+        { id: 6, name: 'System Modelling and Simulation Laboratory' },
+        { id: 7, name: 'Scientific Computing Laboratory' },
+    ];
+
+    export let data: PageData;
+</script>
+
+<section class="space-y-4">
+    <h1 class="h1">Set the quota for each laboratory for this draft process</h1>
+</section>
