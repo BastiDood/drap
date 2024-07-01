edd90b4e57ae18a9188be6832d84415cd9db9f38
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-01 14:52:10 +0800

    feat: implement quota inputs

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,7 +2,7 @@
     import type { PageData } from './$types';
 
     let availableLabs = [
-        { id: 0, name: 'Networks and Distributed Systems Laboratory' },
+        { id: 0, acronym: "ndsl", name: 'Networks and Distributed Systems Laboratory' },
         { id: 1, name: 'Computer Vision & Machine Intelligence Laboratory' },
         { id: 2, name: 'Service Science & Software Engineering Laboratory' },
         { id: 3, name: 'Algorithms & Complexity Laboratory' },
@@ -18,12 +18,27 @@
 <section class="space-y-4">
     <h1 class="h1">Set the quota for each laboratory for this draft process</h1>
     <form method="post">
-        <div class="space-y-2">
-            {#each availableLabs as {id, name}, idx (id)}
-                <div class="card w-full cursor-pointer appearance-none p-4">
-                    {name}
-                </div>
-            {/each}
+        <div class="table-container my-4">
+            <table class="table table-comfortable table-hover">
+                <thead>
+                    <tr>
+                        <th>Laboratory</th>
+                        <th class="w-2/5">Input Quota</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {#each availableLabs as {id, name}, idx (id)}
+                        <tr>
+                            <td>{name}</td>
+                            <td><input type="number" required min=0 placeholder="Quota" class="input variant-form-material py-1 px-2"/></td>
+                        </tr> 
+                    {/each}
+                </tbody>
+            </table>
         </div>
+
+        <button type="submit" class="btn variant-filled">
+            Save Selection
+        </button>
     </form>
 </section>
