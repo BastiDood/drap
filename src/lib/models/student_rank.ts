83a6e30abc10a5314186ccc0464476b0e10a225b
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-02 06:07:03 +0800

    feat: add model for student_rank schema

---
+++ src/lib/models/student_rank.ts
@@ -0,0 +1,9 @@
+import { array, bigint, isoTimestamp, number, object, pipe, string } from "valibot";
+
+export const StudentRank = object({
+    draft_id: bigint(),
+    created_at: pipe(string(), isoTimestamp()),
+    chosen_by: bigint(),
+    labs: array(number()),
+    user_id: string()
+})
