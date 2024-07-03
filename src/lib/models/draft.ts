ee48a73165d73c7fd8c2bcf1c372aa3a36097b5e
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-03 16:34:48 +0800

    fix: change validations on draft model

--- src/lib/models/draft.ts
+++ src/lib/models/draft.ts
@@ -1,10 +1,10 @@
-import { type InferOutput, bigint, isoTimestamp, number, object, pipe, string } from 'valibot';
+import { type InferOutput, bigint, date, minValue, number, object, pipe, safeInteger } from 'valibot';
 
 export const Draft = object({
     draft_id: bigint(),
-    max_rounds: number(),
-    curr_round: number(),
-    created_at: pipe(string(), isoTimestamp()),
+    curr_round: pipe(number(), safeInteger(), minValue(0)),
+    max_rounds: pipe(number(), safeInteger(), minValue(0)),
+    created_at: date(),
 });
 
 export type Draft = InferOutput<typeof Draft>;
