dc9987da8d8af5ecd0747ed251f3756ff56642b7
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-03 22:38:40 +0800

    fix: remove unused lab-member model

--- src/lib/models/lab-member.ts
+++
@@ -1,8 +0,0 @@
-import { type InferOutput, minLength, number, object, pipe, string } from 'valibot';
-
-export const LabMember = object({
-    lab_id: number(),
-    member_id: pipe(string(), minLength(1), minLength(255)),
-});
-
-export type LabMember = InferOutput<typeof LabMember>;
