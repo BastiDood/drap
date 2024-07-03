4e49ff615b6277914846985c99b17a0b57b0e5bd
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-07-03 23:38:23 +0800

    chore: run linter and formatter

--- src/lib/models/student-rank.ts
+++ src/lib/models/student-rank.ts
@@ -1,7 +1,8 @@
-import { type InferOutput, array, date, object, string } from 'valibot';
+import { type InferOutput, array, date, object } from 'valibot';
 import { Draft } from '$lib/models/draft';
 import { FacultyChoice } from '$lib/models/faculty-choice';
 import { Lab } from '$lib/models/lab';
+import { User } from '$lib/models/user';
 
 export const StudentRank = object({
     draft_id: Draft.entries.draft_id,
