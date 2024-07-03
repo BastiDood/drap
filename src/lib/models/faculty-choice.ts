f5d1d60ab893289f1e3c9e12fdde3c122dc9be4d
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-03 22:41:42 +0800

    fix: use foreign key constraint in faculty-choice

--- src/lib/models/faculty-choice.ts
+++ src/lib/models/faculty-choice.ts
@@ -1,11 +1,12 @@
-import { type InferOutput, bigint, date, number, object, string } from 'valibot';
+import { type InferOutput, bigint, date, number, object, pipe, safeInteger } from 'valibot';
+import { User } from './user';
 
 export const FacultyChoice = object({
     choice_id: bigint(),
     created_at: date(),
-    round: number(),
-    faculty_id: string(),
-    lab_id: number(),
+    round: pipe(number(), safeInteger()),
+    faculty_id: User,
+    lab_id: pipe(number(), safeInteger()),
 });
 
 export type FacultyChoice = InferOutput<typeof FacultyChoice>;
