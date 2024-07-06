caf937323d971eff01ae961e3e8a2a2cbdadc441
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-07 00:20:27 +0800

    feat: define email helper functions

---
+++ src/lib/server/email/email.ts
@@ -0,0 +1,10 @@
+
+// this function gets an OAuth access token from Google's OAuth servers on behalf of the service account
+async function getServiceAccountToken() {
+
+}
+
+// this function sends an email to the provided email address via the GMail REST API
+async function sendEmailTo(email: string, body: string) {
+
+}
