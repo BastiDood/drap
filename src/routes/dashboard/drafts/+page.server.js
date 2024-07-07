65963150430c6ac681edf77d80689d3249849eec
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-07-07 22:07:31 +0800

    feat: add button to start new draft

--- src/routes/dashboard/drafts/+page.server.js
+++ src/routes/dashboard/drafts/+page.server.js
@@ -1,7 +1,18 @@
 import { error } from '@sveltejs/kit';
+import { validateString } from '$lib/forms';
 
 export async function load({ locals: { db }, parent }) {
     const { user } = await parent();
     if (!user.is_admin || user.user_id === null || user.lab_id !== null) error(403);
     return { draft: await db.getLatestDraft() };
 }
+
+export const actions = {
+    async default({ locals: { db }, request }) {
+        // TODO: Check if the user has permissions to start a new draft.
+        const data = await request.formData();
+        const rounds = parseInt(validateString(data.get('rounds')), 10);
+        const result = await db.initDraft(rounds);
+        db.logger.info(result);
+    },
+};
