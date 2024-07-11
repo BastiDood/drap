5028abd320dd581dcb1ba444b478520163190136
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-12 01:04:00 +0800

    feat(dashboard/users/+page.server.js): check for is_admin on faculty action

--- src/routes/dashboard/users/+page.server.js
+++ src/routes/dashboard/users/+page.server.js
@@ -10,8 +10,16 @@ export async function load({ locals: { db }, parent }) {
 }
 
 export const actions = {
-    async faculty({ locals: { db }, request }) {
-        // TODO: Verify whether the user is an admin.
+    async faculty({ locals: { db }, cookies, request }) {
+        // [x]: Verify whether the user is an admin.
+        // Resolved: Check if is_admin
+        const sid = cookies.get('sid');
+        if (typeof sid === 'undefined') error(401);
+
+        const user = await db.getUserFromValidSession(sid);
+        if (user === null) error(401);
+        if (!user.is_admin) error(403);
+        
         const data = await request.formData();
         const email = validateString(data.get('email'));
         const lab = validateString(data.get('invite'));
