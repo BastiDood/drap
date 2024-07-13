f9df23ad2920692b98b36477884c4e09f21020d0
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-07-13 08:44:41 +0800

    refactor: prefer `size-` over `w-` and `h-` classes

--- src/lib/alerts/Warning.svelte
+++ src/lib/alerts/Warning.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="alert variant-soft-warning flex flex-row items-center gap-x-2">
-    <Icon {src} class="h-8 w-8" />
+    <Icon {src} class="size-8" />
     <div class="!mt-0 contents">
         <slot />
     </div>
