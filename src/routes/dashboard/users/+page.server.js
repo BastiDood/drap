4d470e914b634b45a892002de736cca4e95d6556
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-07-18 19:05:17 +0800

    feat(admin): implement admin invites

--- src/routes/dashboard/users/+page.server.js
+++ src/routes/dashboard/users/+page.server.js
@@ -10,6 +10,19 @@ export async function load({ locals: { db }, parent }) {
 }
 
 export const actions = {
+    async admin({ locals: { db }, cookies, request }) {
+        const sid = cookies.get('sid');
+        if (typeof sid === 'undefined') error(401);
+
+        const user = await db.getUserFromValidSession(sid);
+        if (user === null) error(401);
+        if (!user.is_admin || user.user_id === null || user.lab_id !== null) error(403);
+
+        const data = await request.formData();
+        const email = validateEmail(data.get('email'));
+        if (await db.inviteNewFacultyOrStaff(email, null)) return;
+        return fail(409);
+    },
     async faculty({ locals: { db }, cookies, request }) {
         const sid = cookies.get('sid');
         if (typeof sid === 'undefined') error(401);
