7c126d780182a4a7b1d93ff481fa32836237ffbd
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-07-18 13:32:42 +0800

    feat(admin): add button for volunteering self as candidate sender

--- src/routes/dashboard/email/+page.svelte
+++ src/routes/dashboard/email/+page.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
     import DesignateForm from './DesignateForm.svelte';
     import ErrorAlert from '$lib/alerts/Error.svelte';
+    import { Icon } from '@steeze-ui/svelte-icon';
+    import { ShieldExclamation } from '@steeze-ui/heroicons';
 
     // eslint-disable-next-line init-declarations
     export let data;
-    $: ({ senders } = data);
+    $: ({
+        senders,
+        user: { email },
+    } = data);
 </script>
 
 {#if senders.length > 0}
     <DesignateForm {senders} />
 {:else}
-    <ErrorAlert
-        >There are no candidate email senders yet. <a href="/oauth/login?extended" class="anchor"
-            >Be the first volunteer yourself as the email dispatcher.</a
-        ></ErrorAlert
-    >
+    <ErrorAlert>There are no candidate email senders yet.</ErrorAlert>
+{/if}
+{#if typeof senders.find(({ email: other }) => other === email) === 'undefined'}
+    <a href="/oauth/login/?extended" class="variant-filled-primary btn btn-lg w-full">
+        <span><Icon src={ShieldExclamation} class="h-12" /></span>
+        <span>Volunteer as a Candidate Sender</span>
+    </a>
 {/if}
