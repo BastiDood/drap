373961b4b6fc5e8f0930f6300c2670ac2bb0921d
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-07-18 21:41:25 +0800

    fix(lab): relax permissions on lab view

--- src/routes/dashboard/lab/+page.server.js
+++ src/routes/dashboard/lab/+page.server.js
@@ -2,6 +2,6 @@ import { error } from '@sveltejs/kit';
 
 export async function load({ locals: { db }, parent }) {
     const { user } = await parent();
-    if (user.is_admin || user.user_id === null || user.lab_id === null) error(403);
+    if (user.user_id === null || user.lab_id === null) error(403);
     return await db.getLabMembers(user.lab_id);
 }
