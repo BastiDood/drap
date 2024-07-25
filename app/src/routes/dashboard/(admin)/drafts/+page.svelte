aa5bf68d2b981aa33c61511c0c9996f2145f9712
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-25 11:01:11 +0800

    feat: move content from main page

--- app/src/routes/dashboard/(admin)/drafts/+page.svelte
+++ app/src/routes/dashboard/(admin)/drafts/+page.svelte
@@ -1,35 +1,18 @@
 <script>
-    import Student from '$lib/users/Student.svelte';
-    import { format } from 'date-fns';
-
-    import ErrorAlert from '$lib/alerts/Error.svelte';
-    import WarningAlert from '$lib/alerts/Warning.svelte';
-
-    import ConcludeForm from './ConcludeForm.svelte';
-    import InterveneForm from './InterveneForm.svelte';
-    import StartForm from './StartForm.svelte';
-    import StudentsPanel from './StudentsPanel.svelte';
-
-    import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
-    import { Icon } from '@steeze-ui/svelte-icon';
-    import { AcademicCap, Beaker, Clock, PaperClip } from '@steeze-ui/heroicons';
-    
     import { goto } from '$app/navigation';
     import { browser } from '$app/environment';
 
     // eslint-disable-next-line init-declarations
     export let data;
-    $: ({ labs } = data);
 
-    let { draft } = data;
-
-    let selectedTab = 0;
+    let { draft, available } = data;
 
     if (browser) {
         if (draft === null) goto("/dashboard/drafts/start");
         else if (draft.curr_round === null) goto("/dashboard/drafts/lottery");
         else if (draft.curr_round > 0) goto("/dashboard/drafts/regular");
-        else goto("/dashboard/drafts/start")
+        else if (available && available.length > 0) goto("/dashboard/drafts/start")
+        else goto("/dashboard/drafts/noregistered")
     }
 </script>
 
