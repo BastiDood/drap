c653ceb4f8b05adc5aceb974ef7c34ffdac679cd
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-08-03 13:51:01 +0800

    fix(email): keep polling for new notifications

--- email/src/main.ts
+++ email/src/main.ts
@@ -18,65 +18,68 @@ const controller = new AbortController();
 process.on('SIGINT', () => controller.abort());
 
 async function listenForDraftNotifications(emailer: Emailer, signal: AbortSignal) {
-    for await (const payload of emailer.db.listen('notify:draft', signal)) {
-        const email = await emailer.db.begin(async db => {
-            const notif = await db.getOneDraftNotification();
-            if (notif === null) return null;
+    for await (const payload of emailer.db.listen('notify:draft', signal))
+        while (true) {
+            const email = await emailer.db.begin(async db => {
+                const notif = await db.getOneDraftNotification();
+                if (notif === null) return null;
 
-            const meta = (() => {
-                switch (notif.ty) {
-                    case 'DraftRoundStarted':
-                        return {
-                            emails: db.getValidFacultyAndStaffEmails(),
-                            subject: `[DRAP] Round #${notif.round} for Draft #${notif.draft_id} has begun!`,
-                            message: `Round #${notif.round} for Draft #${notif.draft_id} has begun. For lab heads, kindly check the students module to see the list of students who have chosen your lab.`,
-                        };
-                    case 'DraftRoundSubmitted':
-                        return {
-                            emails: db.getValidStaffEmails(),
-                            subject: `[DRAP] Acknowledgement from ${notif.lab_id.toUpperCase()} for Round #${notif.round} of Draft #${notif.draft_id}`,
-                            message: `${notif.given_name} ${notif.family_name} has submitted their student preferences on behalf of the ${notif.lab_name} for Round #${notif.round} of Draft #${notif.draft_id}.`,
-                        };
-                    case 'LotteryIntervention':
-                        return {
-                            emails: db.getValidFacultyAndStaffEmails(),
-                            subject: `[DRAP] Lottery Intervention for ${notif.lab_id.toUpperCase()} in Draft #${notif.draft_id}`,
-                            message: ``,
-                        };
-                    default:
-                        return null;
-                }
-            })();
+                const meta = (() => {
+                    switch (notif.ty) {
+                        case 'DraftRoundStarted':
+                            return {
+                                emails: db.getValidFacultyAndStaffEmails(),
+                                subject: `[DRAP] Round #${notif.round} for Draft #${notif.draft_id} has begun!`,
+                                message: `Round #${notif.round} for Draft #${notif.draft_id} has begun. For lab heads, kindly check the students module to see the list of students who have chosen your lab.`,
+                            };
+                        case 'DraftRoundSubmitted':
+                            return {
+                                emails: db.getValidStaffEmails(),
+                                subject: `[DRAP] Acknowledgement from ${notif.lab_id.toUpperCase()} for Round #${notif.round} of Draft #${notif.draft_id}`,
+                                message: `${notif.given_name} ${notif.family_name} has submitted their student preferences on behalf of the ${notif.lab_name} for Round #${notif.round} of Draft #${notif.draft_id}.`,
+                            };
+                        case 'LotteryIntervention':
+                            return {
+                                emails: db.getValidFacultyAndStaffEmails(),
+                                subject: `[DRAP] Lottery Intervention for ${notif.lab_id.toUpperCase()} in Draft #${notif.draft_id}`,
+                                message: ``,
+                            };
+                        default:
+                            return null;
+                    }
+                })();
 
-            assert(meta !== null, 'notify:draft => unexpected notification type');
-            const email = await emailer.send(await meta.emails, meta.subject, meta.message);
-            assert(await db.dropDraftNotification(notif.notif_id), 'cannot drop non-existent notification');
-            return email;
-        });
+                assert(meta !== null, 'notify:draft => unexpected notification type');
+                const email = await emailer.send(await meta.emails, meta.subject, meta.message);
+                assert(await db.dropDraftNotification(notif.notif_id), 'cannot drop non-existent notification');
+                return email;
+            });
 
-        const logger = emailer.db.logger.child({ payload });
-        if (email === null) logger.warn({ email });
-        else logger.info({ email });
-    }
+            if (email === null) break;
+            emailer.db.logger.info({ payload, email });
+        }
 }
 
 async function listenForUserNotifications(emailer: Emailer, signal: AbortSignal) {
-    for await (const payload of emailer.db.listen('notify:user', signal)) {
-        const email = await emailer.db.begin(async db => {
-            const notif = await db.getOneUserNotification();
-            if (notif === null) return null;
-            const email = await emailer.send(
-                [notif.email],
-                `[DRAP] Assigned to ${notif.lab_id.toUpperCase()}`,
-                `Hello, ${notif.given_name} ${notif.given_name}! Kindly note that you have been assigned to the ${notif.lab_name}.`,
-            );
-            assert(await db.dropUserNotification(notif.notif_id), 'cannot drop non-existent notification');
-            return email;
-        });
-        const logger = emailer.db.logger.child({ payload });
-        if (email === null) logger.warn({ email });
-        else logger.info({ email });
-    }
+    for await (const payload of emailer.db.listen('notify:user', signal))
+        while (true) {
+            const email = await emailer.db.begin(async db => {
+                const notif = await db.getOneUserNotification();
+                if (notif === null) return null;
+
+                const email = await emailer.send(
+                    [notif.email],
+                    `[DRAP] Assigned to ${notif.lab_id.toUpperCase()}`,
+                    `Hello, ${notif.given_name} ${notif.given_name}! Kindly note that you have been assigned to the ${notif.lab_name}.`,
+                );
+
+                assert(await db.dropUserNotification(notif.notif_id), 'cannot drop non-existent notification');
+                return email;
+            });
+
+            if (email === null) break;
+            emailer.db.logger.info({ payload, email });
+        }
 }
 
 // Main event loop of the email worker
