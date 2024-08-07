b95712bb08c7f7fff3c6c5751b74c75e5f520afc
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-08-07 15:00:37 +0800

    feat: add route for checking laboratory statuses

--- app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/+layout.svelte
+++ app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
     import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
     import { Icon } from '@steeze-ui/svelte-icon';
-    import { AcademicCap, Beaker, Clock, PaperClip } from '@steeze-ui/heroicons';
+    import { AcademicCap, Beaker, Clock, PaperClip, QueueList } from '@steeze-ui/heroicons';
     import { page } from '$app/stores';
 
     // eslint-disable-next-line init-declarations
@@ -17,11 +17,15 @@
         <Icon src={AcademicCap} slot="lead" class="h-8" />
         <span>Registered Students</span>
     </TabAnchor>
-    <TabAnchor href="/dashboard/drafts/regular/preferences" selected={$page.url.pathname == "/dashboard/drafts/regular/preferences/"} name="tab2" value={1}>
+    <TabAnchor href="/dashboard/drafts/regular/labs" selected={$page.url.pathname == "/dashboard/drafts/regular/labs/"} name="tab2" value={1}>
         <Icon src={Beaker} slot="lead" class="h-8" />
-        <span>Lab Submissions</span>
+        <span>Laboratories</span>
     </TabAnchor>
-    <TabAnchor href="/dashboard/drafts/regular/logs" selected={$page.url.pathname == "/dashboard/drafts/regular/logs/"} name="tab3" value={2}>
+    <TabAnchor href="/dashboard/drafts/regular/preferences" selected={$page.url.pathname == "/dashboard/drafts/regular/preferences/"} name="tab3" value={2}>
+        <Icon src={QueueList} slot="lead" class="h-8" />
+        <span>Labs Preferences</span>
+    </TabAnchor>
+    <TabAnchor href="/dashboard/drafts/regular/logs" selected={$page.url.pathname == "/dashboard/drafts/regular/logs/"} name="tab4" value={3}>
         <Icon src={PaperClip} slot="lead" class="h-8" />
         <span>System Logs</span>
     </TabAnchor>
