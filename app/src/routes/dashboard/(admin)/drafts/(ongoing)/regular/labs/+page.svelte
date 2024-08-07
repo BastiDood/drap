5ba4455d8300471d319543b2b9b96bef2d14d970
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-08-07 16:43:05 +0800

    fix: delete unused components

--- app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/labs/+page.svelte
+++ app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/labs/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { assert } from '$lib/assert.js';
+    import { Accordion } from '@skeletonlabs/skeleton';
+    import LabAccordion from './LabAccordion.svelte';
 
 
     export let data;
@@ -16,3 +18,8 @@
 <!-- Show labs, lab heads, lab quotas, lab membership count as follows -- ( SELECTED / AVAILABLE & PREFERENCE THIS ROUND / AVAILABLE & PREFERENCE FUTURE ROUNDS / QUOTA ) -->
 <!-- Detail already-selected lab members, available students with preferences for the lab in the current round -->
 
+<Accordion>
+    {#each labs as lab}
+        <LabAccordion {available} {selected} {lab} />
+    {/each}
+</Accordion>
