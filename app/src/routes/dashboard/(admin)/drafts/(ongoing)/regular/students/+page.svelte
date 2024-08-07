9e907dfe9c215ca14e73481d9258222a1ff7f702
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-08-07 14:39:31 +0800

    feat: add count of total to students dashboard

--- app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/students/+page.svelte
+++ app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/students/+page.svelte
@@ -5,16 +5,18 @@
     import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
     import { CheckCircle, QuestionMarkCircle } from "@steeze-ui/heroicons";
     import { Icon } from "@steeze-ui/svelte-icon";
+    import { assert } from "$lib/assert";
 
     export let data;
 
     const { available, selected } = data;
+    assert(available != undefined && selected != undefined)
 </script>
 
 <Accordion>
     <AccordionItem>
         <Icon src={CheckCircle} slot="lead" class="h-8"/>
-        <span slot="summary">Available</span>
+        <span slot="summary">Available ({available.length}/{available.length + selected.length})</span>
         <svelte:fragment slot="content">
             {#each available as student}
                 <Student user={student}/>
@@ -23,7 +25,7 @@
     </AccordionItem>
     <AccordionItem>
         <Icon src={QuestionMarkCircle} slot="lead" class="h-8"/>
-        <span slot="summary">Already Selected</span>
+        <span slot="summary">Already Selected ({selected.length}/{available.length + selected.length})</span>
         <svelte:fragment slot="content">
             {#each selected as student}
                 <Student user={student}/>
