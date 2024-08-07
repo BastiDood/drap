e7f06af7153209cb798d60d1a31cb1624cbef635
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-08-07 19:47:35 +0800

    feat: add server file for automation logs route

---
+++ app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/logs/+page.server.ts
@@ -0,0 +1,7 @@
+export async function load({ locals: { db }, parent }) {
+    const { available, selected, draft, labs } = await parent();
+
+    const choiceRecords = await db.getFacultyChoiceRecords(draft.draft_id);
+
+    return { available, selected, draft, labs, choiceRecords }
+}
