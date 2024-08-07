bc744bfe0c77e35fde9ca1e4a1b8908941f172de
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-08-07 14:35:18 +0800

    fix: fix user interface issues with ongoing drafts admin dashboard

--- app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/+layout.svelte
+++ app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/+layout.svelte
@@ -2,30 +2,26 @@
     import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
     import { Icon } from '@steeze-ui/svelte-icon';
     import { AcademicCap, Beaker, Clock, PaperClip } from '@steeze-ui/heroicons';
-    import { assert } from '$lib/assert';
     import { page } from '$app/stores';
 
     // eslint-disable-next-line init-declarations
     export let data;
-    $: ({ labs } = data);
-
-    const { available, selected } = data;
-    assert(available !== undefined && selected !== undefined)
+    let { url: { pathname } } = $page;
 </script>
 
  <!-- TODO: Ongoing Draft (ADMIN DASHBOARD) -->
  <TabGroup>
             
     <!-- Tab Labels -->
-    <TabAnchor href="../students" selected={$page.url.pathname.split("/").reverse()[1] === "students"} name="tab1" value={0}>
+    <TabAnchor href="/dashboard/drafts/regular/students" selected={$page.url.pathname == "/dashboard/drafts/regular/students/"} name="tab1" value={0}>
         <Icon src={AcademicCap} slot="lead" class="h-8" />
         <span>Registered Students</span>
     </TabAnchor>
-    <TabAnchor href="../preferences" selected={$page.url.pathname.split("/").reverse()[1] === "preferences"} name="tab2" value={1}>
+    <TabAnchor href="/dashboard/drafts/regular/preferences" selected={$page.url.pathname == "/dashboard/drafts/regular/preferences/"} name="tab2" value={1}>
         <Icon src={Beaker} slot="lead" class="h-8" />
         <span>Lab Submissions</span>
     </TabAnchor>
-    <TabAnchor href="../logs" selected={$page.url.pathname.split("/").reverse()[1] === "logs"} name="tab3" value={2}>
+    <TabAnchor href="/dashboard/drafts/regular/logs" selected={$page.url.pathname == "/dashboard/drafts/regular/logs/"} name="tab3" value={2}>
         <Icon src={PaperClip} slot="lead" class="h-8" />
         <span>System Logs</span>
     </TabAnchor>
--- app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/+page.svelte
+++ app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/+page.svelte
@@ -1,5 +1,6 @@
 <script>
+    import { browser } from "$app/environment";
     import { goto } from "$app/navigation";
 
-    goto("./preferences")
+    if (browser) goto("/dashboard/drafts/regular/students");
 </script>
