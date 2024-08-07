72197a52a91a4782d49fdf22c343ac248f54dc34
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-08-07 16:01:50 +0800

    fix: use page store variable

--- app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/+layout.svelte
+++ app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/+layout.svelte
@@ -6,26 +6,25 @@
 
     // eslint-disable-next-line init-declarations
     export let data;
-    let { url: { pathname } } = $page;
 </script>
 
  <!-- TODO: Ongoing Draft (ADMIN DASHBOARD) -->
  <TabGroup>
             
     <!-- Tab Labels -->
-    <TabAnchor href="/dashboard/drafts/regular/students" selected={pathname == "/dashboard/drafts/regular/students/"} name="tab1" value={0}>
+    <TabAnchor href="/dashboard/drafts/regular/students" selected={$page.url.pathname == "/dashboard/drafts/regular/students/"} name="tab1" value={0}>
         <Icon src={AcademicCap} slot="lead" class="h-8" />
         <span>Registered Students</span>
     </TabAnchor>
-    <TabAnchor href="/dashboard/drafts/regular/labs" selected={pathname == "/dashboard/drafts/regular/labs/"} name="tab2" value={1}>
+    <TabAnchor href="/dashboard/drafts/regular/labs" selected={$page.url.pathname == "/dashboard/drafts/regular/labs/"} name="tab2" value={1}>
         <Icon src={Beaker} slot="lead" class="h-8" />
         <span>Laboratories</span>
     </TabAnchor>
-    <TabAnchor href="/dashboard/drafts/regular/preferences" selected={pathname == "/dashboard/drafts/regular/preferences/"} name="tab3" value={2}>
+    <TabAnchor href="/dashboard/drafts/regular/preferences" selected={$page.url.pathname == "/dashboard/drafts/regular/preferences/"} name="tab3" value={2}>
         <Icon src={QueueList} slot="lead" class="h-8" />
         <span>Labs Preferences</span>
     </TabAnchor>
-    <TabAnchor href="/dashboard/drafts/regular/logs" selected={pathname == "/dashboard/drafts/regular/logs/"} name="tab4" value={3}>
+    <TabAnchor href="/dashboard/drafts/regular/logs" selected={$page.url.pathname == "/dashboard/drafts/regular/logs/"} name="tab4" value={3}>
         <Icon src={PaperClip} slot="lead" class="h-8" />
         <span>System Logs</span>
     </TabAnchor>
