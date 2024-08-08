2f15568905845910cf0de34c2d2376973ab276d1
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-08-08 21:13:09 +0800

    chore: run formatter

--- app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/logs/+page.server.ts
+++ app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/logs/+page.server.ts
@@ -1,9 +1,9 @@
-import { assert } from "$lib/assert";
+import { assert } from '$lib/assert';
 
 export async function load({ locals: { db }, parent }) {
     const { available, selected, draft, labs } = await parent();
 
-    assert(typeof(draft) !== 'undefined')
+    assert(typeof draft !== 'undefined');
     const choiceRecords = await db.getFacultyChoiceRecords(draft.draft_id);
 
     return { available, selected, draft, labs, choiceRecords };
