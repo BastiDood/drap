b3a97de6cf9e2c3428e7f8fe2d1f6a089a8e5212
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-08-08 21:11:56 +0800

    fix: assert draft is defined in logs server loading

--- app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/logs/+page.server.ts
+++ app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/logs/+page.server.ts
@@ -1,6 +1,9 @@
+import { assert } from "$lib/assert";
+
 export async function load({ locals: { db }, parent }) {
     const { available, selected, draft, labs } = await parent();
 
+    assert(typeof(draft) !== 'undefined')
     const choiceRecords = await db.getFacultyChoiceRecords(draft.draft_id);
 
     return { available, selected, draft, labs, choiceRecords };
