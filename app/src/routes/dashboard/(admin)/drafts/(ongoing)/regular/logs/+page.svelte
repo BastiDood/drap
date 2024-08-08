fab79727f68f1735fe274b5e797757318d1310fb
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-08-08 09:05:54 +0800

    feat: implement rudimentary logging

---
+++ app/src/routes/dashboard/(admin)/drafts/(ongoing)/regular/logs/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let data;
+
+    let { choiceRecords } = data;
+</script>
+
+{#each choiceRecords as choice}
+    <div>
+        Round {choice.round ?? "+"} - {choice.created_at.toUTCString()}:
+        {#if choice.student_email == null}
+            {choice.lab_id.toUpperCase()} has auto-acknowledged or chosen no one for this round
+        {:else}
+            {choice.lab_id.toUpperCase()} has chosen {choice.student_email}
+        {/if}
+    </div>
+{/each}
+
