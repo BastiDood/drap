5d00c1379e4b7cb3c02a15c1021f3137f9dec123
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-08-09 10:44:52 +0800

    fix(draft): ensure that draft cannot be initialized without sufficient quota

--- app/src/routes/dashboard/(admin)/drafts/+page.svelte
+++ app/src/routes/dashboard/(admin)/drafts/+page.svelte
@@ -14,26 +14,27 @@
 </script>
 
 {#if draft === null}
-    {#if labs.some(({ quota }) => quota <= 0)}
+    {#if labs.some(({ quota }) => quota > 0)}
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-[auto_1fr]">
+            <div class="prose dark:prose-invert">
+                <h2>Start a New Draft</h2>
+                <p>
+                    Welcome to the <strong>Draft Ranking Automated Processor</strong>! There are currently no drafts
+                    happening at the moment, but as an administrator, you have the authorization to start a new one.
+                </p>
+                <p>
+                    To begin, simply provide the the maximum number of rounds for the upcoming draft. This has
+                    historically been set to <strong>5</strong>.
+                </p>
+            </div>
+            <InitForm />
+        </div>
+    {:else}
         <WarningAlert
             >The total quota of all labs is currently zero. Please <a href="/dashboard/labs/" class="anchor">allocate</a
             > at least one slot to a lab to proceed.</WarningAlert
         >
     {/if}
-    <div class="grid grid-cols-1 gap-4 sm:grid-cols-[auto_1fr]">
-        <div class="prose dark:prose-invert">
-            <h2>Start a New Draft</h2>
-            <p>
-                Welcome to the <strong>Draft Ranking Automated Processor</strong>! There are currently no drafts
-                happening at the moment, but as an administrator, you have the authorization to start a new one.
-            </p>
-            <p>
-                To begin, simply provide the the maximum number of rounds for the upcoming draft. This has historically
-                been set to <strong>5</strong>.
-            </p>
-        </div>
-        <InitForm />
-    </div>
 {:else if draft.curr_round === null}
     <div class="grid grid-cols-1 gap-4 md:grid-cols-[auto_1fr]">
         <div class="prose dark:prose-invert">
