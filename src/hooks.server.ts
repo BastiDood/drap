ff92114bc569f108c412d80eaf1406f1cfebc1cb
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-06-12 03:33:01 +0800

    feat: define handleError
    This commit adds handleError, lifted from @BastiDood\'s implemention of it in Spectro.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,11 @@
+import { getDotPath, isValiError } from 'valibot';
 import type { PrettyStream } from 'pino-pretty';
 import { pino } from 'pino';
 
 import { dev } from '$app/environment';
 
 import * as POSTGRES from '$lib/server/env/postgres';
+import { AssertionError } from 'assert';
 import { Database } from '$lib/server/database';
 
 // eslint-disable-next-line @typescript-eslint/init-declarations
@@ -43,4 +45,17 @@ export async function handle({ event, resolve }) {
     locals.db.logger.error({ error, response_time: performance.now() - start });
     throw error;
   }
+}
+
+export function handleError({ error, event }) {
+    if (isValiError(error)) {
+        const valibotErrorPaths = error.issues.map(issue => getDotPath(issue)).filter(path => path !== null);
+        event.locals.db.logger.fatal({ valibotErrorPaths }, error.message);
+    } else if (error instanceof AssertionError) {
+        event.locals.db.logger.fatal({ nodeAssertionError: error }, error.message);
+    } else if (error instanceof Error) {
+        event.locals.db.logger.fatal({ error }, error.message);
+    } else {
+        event.locals.db.logger.fatal({ unknownError: error });
+    }
 }
