13db95c4d1ea0395937a27e6892c5214347aa0d5
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-06-29 13:56:07 +0800

    feat: set up lab restore interface

--- src/routes/dashboard/(admin)/labs/+page.svelte
+++ src/routes/dashboard/(admin)/labs/+page.svelte
@@ -4,15 +4,34 @@
 
   import CreateForm from './CreateForm.svelte';
   import QuotaForm from './QuotaForm.svelte';
+  import RestoreForm from './RestoreForm.svelte';
 
   const { data } = $props();
   const { draft, labs } = $derived(data);
+
+  let deletedLabs = $derived(labs.filter((lab) => lab.deletedAt !== null));
+
+  let isRestoreFormVisible = $state(false);
 </script>
 
 {#if typeof draft === 'undefined' || draft.currRound === null}
   <div class="grid grid-cols-1 items-start gap-4 lg:grid-cols-[56ch_1fr]">
     <CreateForm />
-    <QuotaForm {labs} />
+    <div class="flex flex-col w-full gap-y-4">
+      <label class="flex btn preset-filled-surface-500 w-48 items-center" for="see-deleted">
+        <input class="hidden" type="checkbox" name="see-deleted" id="see-deleted" bind:checked={isRestoreFormVisible}>
+        {#if isRestoreFormVisible}
+          <p>Set Lab Quotas</p>
+        {:else}  
+          <p>See Deleted Labs</p>
+        {/if}
+      </label>
+      {#if isRestoreFormVisible}
+        <RestoreForm />
+      {:else}
+        <QuotaForm {labs} />
+      {/if}
+    </div>
   </div>
 {:else if draft.currRound === 0}
   <QuotaForm {labs} />
