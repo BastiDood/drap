971c3a8f478192b8a3226f7e0975612156989f2d
Basti Ortiz <ortiz@bastidood.dev>
2025-07-02 23:59:30 +0800

    fix(app): improve layout of the designated sender
    Closes #55.

--- src/routes/dashboard/email/DesignateForm.svelte
+++ src/routes/dashboard/email/DesignateForm.svelte
@@ -5,7 +5,7 @@
       ? ([
           'demote',
           'preset-filled-warning-500',
-          'preset-tonal-surface border border-surface-500',
+          'preset-tonal-surface outline outline-surface-500',
           'Demote',
           ArrowDown,
         ] as const)
@@ -38,45 +38,43 @@
 <dl class="list-dl">
   {#each senders as { id, email, givenName, familyName, avatarUrl, isActive } (email)}
     {@const [action, variant, card, text, icon] = useSenderControls(isActive)}
-    <div class="card {card} min-w-max">
-      <span><Avatar src={avatarUrl} name="{givenName} {familyName}" size="size-12" /></span>
-      <span class="flex-auto">
+    <div class="card {card} flex min-w-max gap-3 p-2">
+      <Avatar src={avatarUrl} name="{givenName} {familyName}" size="size-12" />
+      <div class="grow">
         <dt><strong><span class="uppercase">{familyName}</span>, {givenName}</strong></dt>
         <dd class="text-sm opacity-50">{email}</dd>
-      </span>
-      <span>
-        <form
-          method="post"
-          class="flex gap-1"
-          use:enhance={() => {
-            disabled = true;
-            return async ({ update }) => {
-              disabled = false;
-              await update();
-            };
-          }}
+      </div>
+      <form
+        method="post"
+        class="flex gap-1"
+        use:enhance={() => {
+          disabled = true;
+          return async ({ update }) => {
+            disabled = false;
+            await update();
+          };
+        }}
+      >
+        <input type="hidden" name="user-id" value={id} />
+        <button
+          type="submit"
+          {disabled}
+          formaction="/dashboard/email/?/{action}"
+          class="{variant} btn btn-sm"
         >
-          <input type="hidden" name="user-id" value={id} />
-          <button
-            type="submit"
-            {disabled}
-            formaction="/dashboard/email/?/{action}"
-            class="{variant} btn btn-sm"
-          >
-            <span><Icon src={icon} class="h-6" /></span>
-            <span>{text}</span>
-          </button>
-          <button
-            type="submit"
-            {disabled}
-            formaction="/dashboard/email/?/remove"
-            class="preset-filled-error-500 btn btn-sm"
-          >
-            <span><Icon src={XMark} class="h-6" /></span>
-            <span>Remove</span>
-          </button>
-        </form>
-      </span>
+          <Icon src={icon} class="h-4" />
+          <span>{text}</span>
+        </button>
+        <button
+          type="submit"
+          {disabled}
+          formaction="/dashboard/email/?/remove"
+          class="preset-filled-error-500 btn btn-sm"
+        >
+          <Icon src={XMark} class="h-4" />
+          <span>Remove</span>
+        </button>
+      </form>
     </div>
   {/each}
 </dl>
