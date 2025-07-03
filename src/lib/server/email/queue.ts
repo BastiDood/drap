2862b53615e0c97e2920ef7d9a0ce99da07142db
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-07-03 15:38:38 +0800

    feat: implement sending email request

--- src/lib/server/email/queue.ts
+++ src/lib/server/email/queue.ts
@@ -1,15 +1,36 @@
 import { BULLMQ_HOST, BULLMQ_PORT } from '$lib/server/env/bullmq';
+import { type Loggable, timed } from '$lib/server/database/decorators';
+import { EmailSendRequest } from '$lib/server/models/email';
+import type { Logger } from 'pino';
 import { Queue } from 'bullmq';
+import { ulid } from 'ulid';
 
-export class EmailQueue {
+export class EmailQueue implements Loggable {
     #queue: Queue;
+    #logger: Logger;
     
-    constructor() {
-        this.#queue = new Queue('emailqueue', {
+    constructor(logger: Logger) {
+        this.#queue = new Queue<EmailSendRequest>('emailqueue', {
             connection: {
                 host: BULLMQ_HOST,
                 port: parseInt(BULLMQ_PORT ?? "", 10)
             }
         })
-    }   
+
+        this.#logger = logger;
+    }
+
+    get logger() {
+        return this.#logger;
+    }
+    
+    @timed async sendEmailRequest(emailRequest: EmailSendRequest) {
+        const requestId = ulid();
+        
+        const job = await this.#queue.add(requestId, emailRequest);
+
+        this.#logger.info({ job })
+
+        return job;
+    }
 }
