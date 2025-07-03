dbde5bc09019f7276d7deec88da505e13edcc73a
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-07-03 16:00:45 +0800

    feat: attach email queue to locals for api routes only

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,7 @@
 declare namespace App {
   interface Locals {
     db: import('$lib/server/database').Database;
+    mailQueue?: import('$lib/server/email/queue').EmailQueue;
     session?: {
       id: import('$lib/server/database').schema.Session['id'];
       user?: import('$lib/server/database').schema.User;
