720f98cd30d5da7d1df2eedbbf58a87844b1910e
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-07-24 17:24:58 +0800

    fix(db): set automation logs to sort by round and lab id

--- src/lib/server/database/index.ts
+++ src/lib/server/database/index.ts
@@ -994,7 +994,7 @@ export class Database implements Loggable {
       )
       .leftJoin(studentUser, eq(schema.facultyChoiceUser.studentUserId, studentUser.id))
       .where(eq(schema.facultyChoice.draftId, draftId))
-      .orderBy(desc(schema.facultyChoice.createdAt));
+      .orderBy(desc(schema.facultyChoice.createdAt), desc(schema.facultyChoice.round), asc(schema.facultyChoice.labId));
   }
 
   @timed async getStudentRanksExport(draftId: bigint) {
