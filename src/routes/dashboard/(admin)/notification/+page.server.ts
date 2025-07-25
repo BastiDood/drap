3c82323a08d06c50b4cf860910ad4e554921e102
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-07-25 21:59:28 +0800

    feat: setup load function for notifications page

---
+++ src/routes/dashboard/(admin)/notification/+page.server.ts
@@ -0,0 +1,17 @@
+import { redirect } from "@sveltejs/kit";
+
+export async function load({ locals: { db, session }, parent }) {
+  if (typeof session?.user === 'undefined') {
+    db.logger.error('attempt to access drafts page without session');
+    redirect(307, '/oauth/login/');
+  }
+
+  const { draft } = await parent();
+  if (typeof draft === 'undefined') {
+    db.logger.warn('no active draft found');
+    return { notifications: [] };
+  }
+
+  const notifications = await db.getNotificationsByDraftId(draft.id);
+  return { notifications };
+}
