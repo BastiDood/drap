7bdb59327bace94e6ade070f1af37f5de26e9135
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-07-26 23:49:48 +0800

    feat: set up Notification component

--- src/routes/dashboard/(admin)/notification/Notification.svelte
+++ src/routes/dashboard/(admin)/notification/Notification.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import type { User } from '$lib/server/database/schema';
+  import { assert } from '$lib/assert';
+  import { type User } from '$lib/server/database/schema';
   import type { Notification } from '$lib/server/models/notification';
 
   export type TargetUser = Pick<User, 'email' | 'avatarUrl' | 'givenName' | 'familyName'>;
@@ -11,18 +12,49 @@
   }
 
   const { data, user, id }: Props = $props();
+
+  function determineTrigger() {
+
+  }
+
+  function determineRecipient() {
+    if (data.target === 'User') {
+      assert(user !== null);
+      return `${user.familyName.toLocaleUpperCase()}, ${user.givenName} <${user.email}>`;
+    } else {
+      switch (data.type) {
+        case 'RoundSubmit':
+          return "All Draft Administrators";
+        default:
+          return "All Draft Administrators and Lab Heads";
+      }
+    }
+  }
+
+  function determineSubject() {
+    if (data.target === 'User') {
+      assert(user !== null);
+      return `Assignment of ${user.familyName}`
+    } 
+  }
+
+  const trigger = $derived.by(determineTrigger);
+  const to = $derived.by(determineRecipient);
+  const subject = $derived.by(determineSubject);
 </script>
 
 <div>
-    <h5 class="h5">
-    {#if data.target === "Draft"}
-        Draft
+  <h5 class="h5">
+    {#if data.target === 'Draft'}
+      Draft
     {:else}
-        Student
+      Student
     {/if}
-        Notification
-    </h5>
-    {#if data.target === "Draft"}
-        
-    {/if}
+    Notification
+  </h5>
+  <div class="flex flex-col">
+    <p><strong>Trigger</strong>: {trigger}</p>
+    <p><strong>To</strong>: {to}</p>
+    <p><strong>Subject</strong>: {subject}</p>
+  </div>
 </div>
