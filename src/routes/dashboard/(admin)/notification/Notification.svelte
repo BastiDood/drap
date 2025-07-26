547ece556f42b9c24055c3364b3cb3e616ff8189
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-07-26 22:33:40 +0800

    feat: add Notification component

---
+++ src/routes/dashboard/(admin)/notification/Notification.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { Notification } from '$lib/server/models/notification';
+
+  interface Props {
+    notification: Notification;
+  }
+
+  const { notification }: Props = $props();
+</script>
