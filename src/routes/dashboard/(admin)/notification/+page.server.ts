b49172244c07058eb03602b5f6b69e7852153d76
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-07-27 18:25:41 +0800

    feat: add check for failure status of job

--- src/routes/dashboard/(admin)/notification/+page.server.ts
+++ src/routes/dashboard/(admin)/notification/+page.server.ts
@@ -1,3 +1,4 @@
+import { getQueue } from '$lib/server/queue/redis.js';
 import { redirect } from '@sveltejs/kit';
 
 export async function load({ locals: { db, session }, parent }) {
@@ -15,11 +16,20 @@ export async function load({ locals: { db, session }, parent }) {
   const notifications = await db.getNotificationsByDraftId(draft.id);
   const notificationRecords = await Promise.all(
     notifications.map(async notification => {
-      const { data } = notification;
+      const { id, data, deliveredAt } = notification;
+      
+      // check the job's status
+      let isFailed = false;
+      if (deliveredAt === null) {
+        const job = await getQueue().getJob(id);
+        // assume that any job in the db but without queue records has failed
+        isFailed = (typeof job === 'undefined' || await job.isFailed())
+      }
+
       // retrieve the user data for notifications that involve it
       if ('userId' in data && typeof data.userId !== 'undefined')
-        return { ...notification, user: await db.getUserById(data.userId) };
-      return { ...notification, user: null };
+        return { ...notification, user: await db.getUserById(data.userId), isFailed };
+      return { ...notification, user: null, isFailed };
     }),
   );
 
