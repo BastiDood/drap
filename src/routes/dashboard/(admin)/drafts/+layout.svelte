52c65f6eb3529aabe71d4de2d4a94a8efac31a08
Ehren Castillo <ehrencastillo@gmail.com>
2025-07-30 00:39:38 +0800

    feat: display registration closing date on drafts page

--- src/routes/dashboard/(admin)/drafts/+layout.svelte
+++ src/routes/dashboard/(admin)/drafts/+layout.svelte
@@ -6,9 +6,11 @@
 </script>
 
 {#if typeof draft !== 'undefined'}
-  {@const { id: draftId, currRound, maxRounds, activePeriodStart } = draft}
+  {@const { id: draftId, currRound, maxRounds, registrationClosesAt, activePeriodStart } = draft}
   {@const startDate = format(activePeriodStart, 'PPP')}
   {@const startTime = format(activePeriodStart, 'pp')}
+  {@const closeDate = format(registrationClosesAt, 'PPP')}
+  {@const closeTime = format(registrationClosesAt, 'pp')}
   <div class="card prose dark:prose-invert max-w-none p-4">
     <p>
       {#if currRound === null}
@@ -19,6 +21,11 @@
         <strong>Draft #{draftId}</strong> is currently on Round <strong>{currRound}</strong>
         of <strong>{maxRounds}</strong>. It opened last <strong>{startDate}</strong> at
         <strong>{startTime}</strong>.
+        {#if registrationClosesAt >= new Date()}
+          Draft registration is currently open and will close on <strong>{closeDate}</strong> at <strong>{closeTime}</strong>.
+        {:else}
+          Draft registration closed on <strong>{closeDate}</strong> at <strong>{closeTime}</strong>.
+        {/if}
       {/if}
     </p>
   </div>
