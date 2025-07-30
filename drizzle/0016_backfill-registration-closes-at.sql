cbb89c1b01789c9756e64462b49db84bde47b5c3
Basti Ortiz <ortiz@bastidood.dev>
2025-07-30 15:16:39 +0800

    docs(db): add reason for custom migration for registration close date

--- drizzle/0016_backfill-registration-closes-at.sql
+++ drizzle/0016_backfill-registration-closes-at.sql
@@ -1,1 +1,4 @@
+-- We treat the upper bound of the active period (i.e., the draft conclusion date) as the
+-- registration close because we technically did only close the registration only during the draft
+-- conclusion (thanks to a bug in the user interface).
 UPDATE "drap"."draft" SET "registration_closes_at" = upper("active_period") WHERE upper("drap"."draft"."active_period") IS NOT NULL;
