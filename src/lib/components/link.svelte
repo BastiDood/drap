eb141aa1490e771b01bb4f53fe295d033853023d
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2025-08-13 04:28:05 +0800

    feat(ui): add link component

---
+++ src/lib/components/link.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { HTMLAnchorAttributes } from 'svelte/elements';
+
+  const { children, ...rest }: HTMLAnchorAttributes = $props();
+</script>
+
+<a
+  {...rest}
+  class="hover:text-secondary-500 decoration-surface-400 underline underline-offset-3 transition"
+  >{@render children?.()}</a
+>
