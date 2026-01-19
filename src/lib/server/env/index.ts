98cc9973bd0a5a7b76de911a3c19ac5730cbeae6
Basti Ortiz <ortiz@bastidood.dev>
2026-01-19 22:02:56 +0800

    chore(env): remove unused environment variables

--- src/lib/server/env/index.ts
+++
@@ -1,9 +0,0 @@
-import { env } from '$env/dynamic/private';
-
-// eslint-disable-next-line @typescript-eslint/init-declarations
-let jobConcurrency: number | undefined;
-if (typeof env.JOB_CONCURRENCY !== 'undefined')
-  jobConcurrency = Number.parseInt(env.JOB_CONCURRENCY, 10);
-
-/** Maximum number of email worker jobs to run concurrently. */
-export const JOB_CONCURRENCY = jobConcurrency;
