f3c9c9eedbf96839d242a5cb36fbe348befa56cb
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-01-21 00:29:20 +0800

    refactor: stack hero buttons on mobile

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,6 +1,6 @@
 <script>
   import ArrowRight from '@lucide/svelte/icons/arrow-right';
-  import ScrollText from '@lucide/svelte/icons/scroll-text'
+  import ScrollText from '@lucide/svelte/icons/scroll-text';
 
   import DCS from '$lib/assets/dcs.webp?url';
   import Logo from '$lib/assets/logo-DRAP-colored.svg';
@@ -8,21 +8,28 @@
 </script>
 
 <div
-  class="mt-4 flex min-h-104 flex-col items-center rounded-2xl bg-[rgb(30,79,65)] text-center bg-blend-multiply p-6 not-prose justify-center"
+  class="not-prose mt-4 flex min-h-104 flex-col items-center justify-center rounded-2xl bg-[rgb(30,79,65)] p-6 text-center bg-blend-multiply"
   style="background-image: url({DCS})"
 >
   <img src={Logo} alt="Logo of DRAP" class="w-4/5 max-w-136 md:-mb-8" />
   <div>
-    <h1 class="text-surface-50 font-black text-2xl md:text-4xl mb-8">Draft Ranking Automated Processor</h1>
-    <div class="gap-2 flex *:w-fit items-center justify-center">
-        <a href={resolve('/dashboard/oauth/login')} class="btn btn-xl preset-filled-primary-500 px-4 py-2">
-            <ArrowRight />
-            <span> Go to Dashboard </span>
-        </a>
-        <a href={resolve('/history/')} class="btn btn-xl preset-tonal-surface px-4 py-2">
-            <ScrollText />
-            <span> View Draft </span>
-        </a>
+    <h1 class="text-surface-50 mb-8 text-2xl font-black md:text-4xl">
+      Draft Ranking Automated Processor
+    </h1>
+    <div
+      class="flex flex-col items-center justify-center gap-4 *:w-3/5 *:min-w-fit md:flex-row md:gap-2 *:md:w-auto"
+    >
+      <a
+        href={resolve('/dashboard/oauth/login')}
+        class="btn btn-xl preset-filled-primary-500 px-4 py-2"
+      >
+        <ArrowRight />
+        <span> Go to Dashboard </span>
+      </a>
+      <a href={resolve('/history/')} class="btn btn-xl preset-tonal-surface px-4 py-2">
+        <ScrollText />
+        <span> View Draft </span>
+      </a>
     </div>
   </div>
 </div>
