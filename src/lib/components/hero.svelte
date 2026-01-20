d52d5ee96ea121e4c34a283d50e05f1537e2c209
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-01-20 22:54:22 +0800

    feat(ui): add cta buttons to hero

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,20 +1,28 @@
 <script>
-  import CircleUser from '@lucide/svelte/icons/circle-user';
+  import ArrowRight from '@lucide/svelte/icons/arrow-right';
+  import ScrollText from '@lucide/svelte/icons/scroll-text'
 
   import DCS from '$lib/assets/dcs.webp?url';
   import Logo from '$lib/assets/logo-DRAP-colored.svg';
+  import { resolve } from '$app/paths';
 </script>
 
 <div
-  class="m-6 mt-6 flex h-104 flex-col items-center rounded-2xl bg-[rgb(30,79,65)] text-center bg-blend-multiply"
+  class="mt-4 flex min-h-104 flex-col items-center rounded-2xl bg-[rgb(30,79,65)] text-center bg-blend-multiply p-6 not-prose justify-center"
   style="background-image: url({DCS})"
 >
-  <img src={Logo} alt="Logo of DRAP" class="-mb-8 size-2/3" />
-  <div class="mb-8">
-    <h1 class="text-foreground">Draft Ranking Automated Processor</h1>
-    <a href="/oauth/login/" rel="external" class="btn btn-xl preset-filled-primary-500">
-      <CircleUser />
-      <span> Sign in with UP Mail </span>
-    </a>
+  <img src={Logo} alt="Logo of DRAP" class="w-4/5 max-w-136 md:-mb-8" />
+  <div>
+    <h1 class="text-surface-50 font-black text-2xl md:text-4xl mb-8">Draft Ranking Automated Processor</h1>
+    <div class="gap-2 flex *:w-fit items-center justify-center">
+        <a href={resolve('/dashboard/oauth/login')} class="btn btn-xl preset-filled-primary-500 px-4 py-2">
+            <ArrowRight />
+            <span> Go to Dashboard </span>
+        </a>
+        <a href={resolve('/history/')} class="btn btn-xl preset-tonal-surface px-4 py-2">
+            <ScrollText />
+            <span> View Draft </span>
+        </a>
+    </div>
   </div>
 </div>
