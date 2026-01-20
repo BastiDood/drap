bd6ccf46a691cb0f4c0293e9db213ff0ebd2d658
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-01-21 01:20:50 +0800

    refactor: add hero background image url to tailwind config

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -2,14 +2,12 @@
   import ArrowRight from '@lucide/svelte/icons/arrow-right';
   import ScrollText from '@lucide/svelte/icons/scroll-text';
 
-  import DCS from '$lib/assets/dcs.webp?url';
   import Logo from '$lib/assets/logo-DRAP-colored.svg';
   import { resolve } from '$app/paths';
 </script>
 
 <div
-  class="not-prose inset-shadow-surface-900 m-6 flex min-h-104 flex-col items-center justify-center rounded-2xl bg-[rgb(30,79,65)] p-6 text-center bg-blend-multiply inset-shadow-sm"
-  style="background-image: url({DCS})"
+  class="not-prose inset-shadow-surface-900 bg-dcs m-6 flex min-h-104 flex-col items-center justify-center rounded-2xl bg-[rgb(30,79,65)] bg-cover bg-center p-6 text-center bg-blend-multiply inset-shadow-sm"
 >
   <img src={Logo} alt="Logo of DRAP" class="w-4/5 max-w-136 md:-mb-8" />
   <div>
