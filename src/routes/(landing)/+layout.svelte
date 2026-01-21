6720f39a966e779d69347aaef8942a0a29e77530
Basti Ortiz <ortiz@bastidood.dev>
2026-01-21 17:12:02 +0800

    fix(ui): disable top bar for dashboard

---
+++ src/routes/(landing)/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Footer from '$lib/components/footer.svelte';
+  import Navbar from '$lib/components/navbar.svelte';
+
+  const { children } = $props();
+</script>
+
+<div class="h-dvh overflow-y-auto">
+  <Navbar />
+  {@render children?.()}
+  <Footer />
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,6 @@
   import { ModeWatcher } from 'mode-watcher';
 
   import banner from '$lib/banner.png?url';
-  import Footer from '$lib/components/footer.svelte';
-  import Navbar from '$lib/components/navbar.svelte';
   import { ORIGIN } from '$lib/env';
 
   import './app.css';
@@ -17,8 +15,4 @@
   <meta property="og:image" content="{ORIGIN}{banner}" />
 </svelte:head>
 
-<div class="h-dvh overflow-y-auto">
-  <Navbar />
-  {@render children?.()}
-  <Footer />
-</div>
+{@render children?.()}
