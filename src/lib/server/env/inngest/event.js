e5b4f3a027056bb09dfa7b4c097604619f2b5464
Basti Ortiz <ortiz@bastidood.dev>
2026-01-21 20:45:22 +0800

    fix: use Inngest keys only optionally during production

--- src/lib/server/env/inngest/event.js
+++ src/lib/server/env/inngest/event.js
@@ -1,9 +1,9 @@
 import assert from 'node:assert/strict';
 
-import { building } from '$app/environment';
+import { building, dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
 
-if (building) {
+if (dev || building) {
   // It doesn't make sense to assert environment variables at build-time.
 } else {
   assert(typeof env.INNGEST_EVENT_KEY !== 'undefined');
