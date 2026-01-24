060207a00eec6983da59762deb390a1ecd8d507d
Basti Ortiz <ortiz@bastidood.dev>
2026-01-25 04:55:27 +0800

    feat(ui): add `collapsible` component

---
+++ src/lib/components/ui/collapsible/index.js
@@ -0,0 +1,13 @@
+import Content from './collapsible-content.svelte';
+import Root from './collapsible.svelte';
+import Trigger from './collapsible-trigger.svelte';
+
+export {
+  Root,
+  Content,
+  Trigger,
+  //
+  Root as Collapsible,
+  Content as CollapsibleContent,
+  Trigger as CollapsibleTrigger,
+};
