dd2d9813514bd40a8f32d80075047d2e4acfdd91
Basti Ortiz <ortiz@bastidood.dev>
2026-01-27 02:07:31 +0800

    feat(ui): add `dropdown-menu`

---
+++ src/lib/components/ui/dropdown-menu/dropdown-menu-group-heading.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { ComponentProps } from 'svelte';
+  import { DropdownMenu as DropdownMenuPrimitive } from 'bits-ui';
+
+  import { cn } from '$lib/components/ui/utils';
+
+  interface Props extends ComponentProps<typeof DropdownMenuPrimitive.GroupHeading> {
+    inset?: boolean;
+  }
+
+  let { ref = $bindable(null), class: className, inset, ...restProps }: Props = $props();
+</script>
+
+<DropdownMenuPrimitive.GroupHeading
+  bind:ref
+  data-slot="dropdown-menu-group-heading"
+  data-inset={inset}
+  class={cn('px-2 py-1.5 text-sm font-semibold data-inset:ps-8', className)}
+  {...restProps}
+/>
---
+++ src/lib/components/ui/dropdown-menu/dropdown-menu-label.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  import { cn, type WithElementRef } from '$lib/components/ui/utils';
+
+  interface Props extends WithElementRef<HTMLAttributes<HTMLDivElement>> {
+    inset?: boolean;
+  }
+
+  let { ref = $bindable(null), class: className, inset, children, ...restProps }: Props = $props();
+</script>
+
+<div
+  bind:this={ref}
+  data-slot="dropdown-menu-label"
+  data-inset={inset}
+  class={cn('px-2 py-1.5 text-sm font-semibold data-inset:ps-8', className)}
+  {...restProps}
+>
+  {@render children?.()}
+</div>
