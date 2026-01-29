9ab67239cbd5c2a488bad879dfca804ee26e1a5e
Basti Ortiz <ortiz@bastidood.dev>
2026-01-30 05:01:37 +0800

    chore: prefer `unknown` over `any`

--- src/lib/assert.ts
+++ src/lib/assert.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function assert(condition: any, msg = 'assertion failed'): asserts condition {
+export function assert(condition: unknown, msg = 'assertion failed'): asserts condition {
   if (!condition) throw new Error(msg);
 }
