3e28e739f47cc20e26a0df75da068c8e7b755197
Basti Ortiz <ortiz@bastidood.dev>
2026-01-30 06:21:02 +0800

    fix(app): allow faculty members to view the lab page
    Fixes #108. The root cause is due to a privacy guard that prevents students from viewing lab members outside their draft class. This inadvertently breaks lab viewing for non-admin faculty members.

--- src/routes/dashboard/lab/+page.server.ts
+++ src/routes/dashboard/lab/+page.server.ts
@@ -2,18 +2,20 @@ import { error, redirect } from '@sveltejs/kit';
 
 import { db, getDrafts, getLabMembers, getUserLabAssignmentDraftId } from '$lib/server/database';
 import { Logger } from '$lib/server/telemetry/logger';
+import { Tracer } from '$lib/server/telemetry/tracer';
 
 const SERVICE_NAME = 'routes.dashboard.lab';
 const logger = Logger.byName(SERVICE_NAME);
+const tracer = Tracer.byName(SERVICE_NAME);
 
 export async function load({ locals: { session } }) {
   if (typeof session?.user === 'undefined') {
-    logger.warn('attempt to access lab page without session');
+    logger.error('attempt to access lab page without session');
     redirect(307, '/dashboard/oauth/login');
   }
 
   if (session.user.googleUserId === null || session.user.labId === null) {
-    logger.warn('insufficient permissions to access lab page', {
+    logger.error('insufficient permissions to access lab page', void 0, {
       isAdmin: session.user.isAdmin,
       googleUserId: session.user.googleUserId,
       labId: session.user.labId,
@@ -21,38 +23,43 @@ export async function load({ locals: { session } }) {
     error(403);
   }
 
-  type LabInfo = Awaited<ReturnType<typeof getLabMembers>>;
-  // eslint-disable-next-line @typescript-eslint/init-declarations
-  let info: LabInfo;
+  const {
+    id: sessionId,
+    user: { id: userId, isAdmin, googleUserId, labId },
+  } = session;
 
-  if (session.user.isAdmin) {
-    info = await getLabMembers(db, session.user.labId);
-  } else {
-    const userLatestDraft = await getUserLabAssignmentDraftId(
-      db,
-      session.user.id,
-      session.user.labId,
-    );
-    if (typeof userLatestDraft === 'undefined') {
-      // This is a required check to ensure that the user can only see lab members of their own
-      // draft class. Otherwise, every student can see all lab members, which poor privacy practice.
-      logger.error('student without prior draft results attempted to access lab', void 0, {
-        userId: session.user.id,
-        labId: session.user.labId,
-      });
-      error(403);
+  return await tracer.asyncSpan('load-lab-page', async span => {
+    span.setAttributes({
+      'session.id': sessionId,
+      'session.user.id': userId,
+      'session.user.is_admin': isAdmin,
+    });
+    if (labId !== null) span.setAttribute('session.user.lab_id', labId);
+    if (googleUserId !== null) span.setAttribute('session.user.google_id', googleUserId);
+
+    // eslint-disable-next-line @typescript-eslint/init-declarations
+    let info: Awaited<ReturnType<typeof getLabMembers>>;
+    if (isAdmin) {
+      logger.debug('admin accessing the lab page');
+      info = await getLabMembers(db, labId);
+    } else {
+      const userLatestDraft = await getUserLabAssignmentDraftId(db, userId, labId);
+      if (typeof userLatestDraft === 'undefined') {
+        // The assumption here is that the user was invited as a faculty member
+        // (and thus has no prior draft results).
+        logger.debug('faculty accessing the lab page');
+        info = await getLabMembers(db, labId);
+      } else {
+        // If the user was invited to this lab via regular draft methods, they are treated as a
+        // student/researcher of the lab. This is a required check to ensure that the user can only
+        // see lab members of their own draft class. Otherwise, every student can see all lab members
+        // of all time, which is poor data privacy hygiene.
+        logger.debug('researcher accessing the lab page');
+        info = await getLabMembers(db, labId, userLatestDraft.draftId);
+      }
     }
-    const { draftId: userLatestDraftId } = userLatestDraft;
-    info = await getLabMembers(db, session.user.labId, userLatestDraftId);
-  }
 
-  await getLabMembers(db, session.user.labId);
-  logger.debug('lab info fetched', {
-    labName: info.lab,
-    headCount: info.heads.length,
-    memberCount: info.members.length,
+    const drafts = await getDrafts(db);
+    return { ...info, drafts };
   });
-
-  const drafts = await getDrafts(db);
-  return { ...info, drafts };
 }
