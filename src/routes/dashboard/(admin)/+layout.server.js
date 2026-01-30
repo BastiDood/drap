1d8f485d8cccc1abfa3a6d9053e40834ed641223
Basti Ortiz <ortiz@bastidood.dev>
2026-01-30 21:20:06 +0800

    refactor(app): move modules around in new layout groups

--- src/routes/dashboard/(admin)/+layout.server.js
+++
@@ -1,8 +0,0 @@
-import { db, getActiveDraft } from '$lib/server/database';
-
-export async function load() {
-  return {
-    draft: await getActiveDraft(db),
-    requestedAt: new Date(),
-  };
-}
--- src/routes/dashboard/(draft)/+layout.server.js
+++ src/routes/dashboard/(draft)/+layout.server.js
@@ -1,25 +1,8 @@
-import { error } from '@sveltejs/kit';
-
 import { db, getActiveDraft } from '$lib/server/database';
-import { Logger } from '$lib/server/telemetry/logger';
-
-const SERVICE_NAME = 'routes.dashboard.draft.layout';
-const logger = Logger.byName(SERVICE_NAME);
 
 export async function load() {
-  const draft = await getActiveDraft(db);
-  if (typeof draft === 'undefined') {
-    logger.error('no active draft found');
-    error(499);
-  }
-
-  logger.info('active draft found', {
-    id: draft.id.toString(),
-    currRound: draft.currRound,
-    maxRounds: draft.maxRounds,
-  });
-
-  const requestedAt = new Date();
-
-  return { draft, requestedAt };
+  return {
+    draft: await getActiveDraft(db),
+    requestedAt: new Date(),
+  };
 }
