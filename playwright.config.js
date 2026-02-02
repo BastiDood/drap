e2602a4f81e2c68def61c2060f68bc0716fdd856
Basti Ortiz <ortiz@bastidood.dev>
2026-02-02 19:49:21 +0800

    test: only allow a single worker for global database setups

--- playwright.config.js
+++ playwright.config.js
@@ -4,6 +4,8 @@ export default defineConfig({
   name: 'E2E',
   testDir: './tests',
   outputDir: './output',
+  // Only a single worker so that the database fixtures are shared across all tests.
+  workers: 1,
   fullyParallel: true,
   // Assumes that `pnpm build` has already been run.
   webServer: { command: 'pnpm preview', port: 4173 },
