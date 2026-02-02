dfdbf8ded5e92c7d3b2cc9ca14deddd042e0f50e
Basti Ortiz <ortiz@bastidood.dev>
2026-02-02 19:50:03 +0800

    test: remove assertions for Inngest variables

--- src/lib/server/env/inngest/signing.js
+++ src/lib/server/env/inngest/signing.js
@@ -1,12 +1,3 @@
-import assert from 'node:assert/strict';
-
-import { building, dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
-
-if (dev || building) {
-  // It doesn't make sense to assert environment variables at build-time.
-} else {
-  assert(typeof env.INNGEST_SIGNING_KEY !== 'undefined');
-}
 
 export const SIGNING_KEY = env.INNGEST_SIGNING_KEY;
