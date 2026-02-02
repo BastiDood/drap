8e8803c60a5cb77c6e8bc1c54e7fdc7cbee4098f
Basti Ortiz <ortiz@bastidood.dev>
2026-02-02 20:38:50 +0800

    chore: add graceful shutdown to Playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
   // Only a single worker so that the database fixtures are shared across all tests.
   workers: 1,
   fullyParallel: true,
-  // Assumes that `pnpm build` has already been run.
-  webServer: { command: 'pnpm preview', port: 4173 },
+  webServer: {
+    // Assumes that `pnpm build` has already been run.
+    command: 'pnpm preview',
+    port: 4173,
+    gracefulShutdown: { signal: 'SIGINT', timeout: 0 },
+  },
 });
