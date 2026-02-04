048ac3d147b12e24fa477479c28db8d2c5fef660
Basti Ortiz <ortiz@bastidood.dev>
2026-02-05 02:56:40 +0800

    feat(ui): add `native-select` component

---
+++ src/lib/components/ui/native-select/native-select-opt-group.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { HTMLOptgroupAttributes } from 'svelte/elements';
+
+  import type { WithElementRef } from '$lib/components/ui/utils';
+
+  let {
+    ref = $bindable(null),
+    children,
+    ...restProps
+  }: WithElementRef<HTMLOptgroupAttributes> = $props();
+</script>
+
+<optgroup bind:this={ref} data-slot="native-select-opt-group" {...restProps}>
+  {@render children?.()}
+</optgroup>
