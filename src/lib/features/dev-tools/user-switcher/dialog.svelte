116830c6e4c27b1af501c5ba414833877cddae0b
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-19 18:22:26 +0800

    feat(dev-tools): add switch user dialog

---
+++ src/lib/features/dev-tools/user-switcher/dialog.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import * as Dialog from '$lib/components/ui/dialog';
+
+  import UserForm from './form.svelte';
+
+  interface Props {
+    open: boolean;
+  }
+
+  let { open = $bindable() }: Props = $props();
+
+  function handleSuccess() {
+    open = false;
+  }
+</script>
+
+<Dialog.Root bind:open>
+  <Dialog.Content class="sm:max-w-md">
+    <Dialog.Header>
+      <Dialog.Title>Switch to Another User</Dialog.Title>
+      <Dialog.Description>
+        Switch to another logged-in user for testing purposes only. This tool is unavailable in
+        production.
+      </Dialog.Description>
+    </Dialog.Header>
+    <UserForm onSuccess={handleSuccess} />
+  </Dialog.Content>
+</Dialog.Root>
