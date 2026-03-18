9a5c5491b4d7bbd9cc44af5ca9d39edf0089620c
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-18 22:09:08 +0800

    feat(drafts): provide query client to `DraftTimeline`

--- src/routes/dashboard/(draft)/drafts/[draftId]/+page.svelte
+++ src/routes/dashboard/(draft)/drafts/[draftId]/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
   import { DraftTimeline } from '$lib/features/drafts';
 
+  import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+
   const { data } = $props();
+
+  // Make new TanStack query client
+  const queryClient = new QueryClient();
 </script>
 
-<DraftTimeline {...data} />
+<QueryClientProvider client={queryClient}>
+  <DraftTimeline {...data} />
+</QueryClientProvider>
