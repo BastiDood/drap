9ee383cab83f06ae835bf6454a095707e6ca9a3c
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-20 12:48:20 +0800

    feat(drafts/draftees): make sort button for header

---
+++ src/lib/features/drafts/draftees/sort-by-header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { ComponentProps } from 'svelte';
+  import ArrowUpDownIcon from '@lucide/svelte/icons/arrow-up-down';
+
+  import { Button } from '$lib/components/ui/button';
+
+  interface Props extends ComponentProps<typeof Button> {
+    header: string;
+  }
+
+  const { header, variant = 'ghost', ...restProps }: Props = $props();
+</script>
+
+<Button {variant} {...restProps}>
+  {header}
+  <ArrowUpDownIcon class="h-5 w-5" />
+</Button>
