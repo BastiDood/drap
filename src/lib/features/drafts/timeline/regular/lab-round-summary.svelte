dd7a62f670c8d6ce21b65e0ee0c7c7fa5b3e0052
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-20 13:44:53 +0800

    feat(drafts/timeline/regular): make `LabRoundSummary`

---
+++ src/lib/features/drafts/timeline/regular/lab-round-summary.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { Badge } from '$lib/components/ui/badge';
+
+  import Draftees from '../../draftees/index.svelte';
+
+  import type { Lab } from '$lib/features/drafts/types';
+
+  interface Props {
+    draftId: bigint;
+    round: number;
+    lab: Lab;
+  }
+
+  const { draftId, round, lab }: Props = $props();
+</script>
+
+<div class="flex w-full items-center justify-between h-12 border-b last:border-0 pb-1">
+  {#if lab.quota === 0}
+    <h5 class="text-muted-foreground text-lg font-medium">{lab.name}</h5>
+  {:else}
+    <div class="flex items-center gap-1">
+      <h5 class="text-lg font-medium">{lab.name}</h5>
+      <Badge variant="outline" class="border-warning bg-warning/10 font-mono text-xs uppercase h-fit">
+        {lab.quota} maximum
+      </Badge>
+    </div>
+  {/if}
+  <div class="flex items-center gap-1">
+    <!-- Members -->
+    <Draftees {draftId} {lab} queryKey={`round-${round}-${lab.name}-members`} mustShowDrafted={true}>
+      {#snippet trigger()}
+        <Badge variant="outline" class="border-primary bg-primary/10 font-mono text-xs uppercase h-fit">
+          See Members
+        </Badge>
+      {/snippet}
+    </Draftees>
+
+    <!-- Preferred -->
+    <Draftees {draftId} {round} {lab} queryKey={`round-${round}-${lab.name}-preferred`} mustShowDrafted={false}>
+      {#snippet trigger()}
+        <Badge variant="outline" class="border-accent bg-accent/10 font-mono text-xs uppercase h-fit">
+          See Preferred
+        </Badge>
+      {/snippet}
+    </Draftees>
+
+    <!-- Interested -->
+    <Draftees {draftId} {round} {lab} queryKey={`round-${round}-${lab.name}-interested`} mustShowDrafted={false} mustShowInterest={true}>
+      {#snippet trigger()}
+        <Badge variant="outline" class="border-secondary bg-secondary/10 font-mono text-xs uppercase h-fit">
+          See Interested
+        </Badge>
+      {/snippet}
+    </Draftees>
+  </div>
+</div>
