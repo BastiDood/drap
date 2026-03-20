1bbbe50ab153eef28dca3eb05ec86f817445eae3
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-20 17:32:01 +0800

    feat(drafts/draftees): authenticate user

--- src/routes/dashboard/(draft)/drafts/[draftId]/draftees/+server.ts
+++ src/routes/dashboard/(draft)/drafts/[draftId]/draftees/+server.ts
@@ -1,10 +1,19 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 
 import { db } from '$lib/server/database';
 import { getStudentsInDraftTaggedByLab } from '$lib/server/database/drizzle';
 import type { SerializableStudent } from '$lib/features/drafts/types';
 
-export async function GET({ params }) {
+export async function GET({ params, locals: { session } }) {
+  if (typeof session?.user === 'undefined') {
+    error(401);
+  }
+
+  const { user } = session;
+  if (!user.isAdmin || user.googleUserId === null || user.labId !== null) {
+    error(403);
+  }
+
   const draftId = BigInt(params.draftId);
 
   const draftees = await getStudentsInDraftTaggedByLab(db, draftId);
