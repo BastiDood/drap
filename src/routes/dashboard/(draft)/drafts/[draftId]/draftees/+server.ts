e1a890e45a9b2cfc1db7dbf3e31d50ba14a17fe1
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-20 17:48:18 +0800

    feat(drafts/draftees): log draftee list fetches

--- src/routes/dashboard/(draft)/drafts/[draftId]/draftees/+server.ts
+++ src/routes/dashboard/(draft)/drafts/[draftId]/draftees/+server.ts
@@ -1,28 +1,62 @@
 import { error, json } from '@sveltejs/kit';
 
 import { db } from '$lib/server/database';
-import { getStudentsInDraftTaggedByLab } from '$lib/server/database/drizzle';
+import { getStudentsInDraftTaggedByLab } from '$lib/server/database/drizzle'
+import { Logger } from '$lib/server/telemetry/logger';
+import { Tracer } from '$lib/server/telemetry/tracer';;
 import type { SerializableStudent } from '$lib/features/drafts/types';
 
+const SERVICE_NAME = 'routes.dashboard.admin.drafts.draftees';
+const logger = Logger.byName(SERVICE_NAME);
+const tracer = Tracer.byName(SERVICE_NAME);
+
 export async function GET({ params, locals: { session } }) {
-  if (typeof session?.user === 'undefined') error(401);
+  if (typeof session?.user === 'undefined') {
+    logger.fatal('attempt to fetch draftee list without session');
+    error(401);
+  }
 
   const { user } = session;
-  if (!user.isAdmin || user.googleUserId === null || user.labId !== null) error(403);
+  if (!user.isAdmin || user.googleUserId === null || user.labId !== null) {
+    logger.error('insufficient permissions to fetch draftee list', void 0, {
+      'user.is_admin': user.isAdmin,
+      'user.google_id': user.googleUserId,
+      'user.lab_id': user.labId,
+    });
+    error(403);
+  }
 
-  const draftId = BigInt(params.draftId);
+  const {
+    id: sessionId,
+    user: { id: userId },
+  } = session;
 
-  const draftees = await getStudentsInDraftTaggedByLab(db, draftId);
+  return await tracer.asyncSpan('fetch-draftee-list', async span => {
+    span.setAttributes({
+      'session.id': sessionId,
+      'session.user.id': userId,
+      'draft.id': params.draftId,
+    });
 
-  // Make data JSON-serializable
-  const serializableDrafteeList = draftees.map(draftee => {
-    return {
-      ...draftee,
+    const draftId = BigInt(params.draftId);
 
-      // Reassign non-serializable attributes
-      studentNumber: draftee.studentNumber === null ? null : draftee.studentNumber.toString(),
-    };
-  }) as SerializableStudent[];
+    const draftees = await getStudentsInDraftTaggedByLab(db, draftId);
 
-  return json(serializableDrafteeList);
+    // Make data JSON-serializable
+    const serializableDrafteeList = draftees.map(draftee => {
+      return {
+        ...draftee,
+
+        // Reassign non-serializable attributes
+        studentNumber: draftee.studentNumber === null ? null : draftee.studentNumber.toString(),
+      };
+    }) as SerializableStudent[];
+
+    logger.debug('draftees fetched', {
+      'draft.id': draftId.toString(),
+      'draftee-list.length': serializableDrafteeList.length,
+    });
+
+    return json(serializableDrafteeList);
+  });
 }
