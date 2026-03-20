f9b9a94176f1dc0a337e06a721dc730366b56d50
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-20 14:22:51 +0800

    feat(drafts/timeline/lottery/lottery-section): export loader to drawer component

---
+++ src/lib/features/drafts/timeline/lottery/lottery-section/index.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button';
+  import * as Drawer from '$lib/components/ui/drawer';
+
+  import type { Lab } from '$lib/features/drafts/types';
+
+  import Loader from './loader.svelte';
+
+  interface Props {
+    draftId: bigint;
+    labs: Pick<Lab, 'id' | 'name'>[];
+  }
+
+  const { draftId, labs }: Props = $props();
+</script>
+
+<Drawer.Root>
+  <Drawer.Trigger>
+    <Button variant="outline" class="border-warning text-warning">
+      Eligible for Lottery
+    </Button>
+  </Drawer.Trigger>
+  <Drawer.Content class="min-h-screen">
+    <div class="overflow-auto pb-40 px-8">
+      <Loader {draftId} {labs} />
+    </div>
+  </Drawer.Content>
+</Drawer.Root>
