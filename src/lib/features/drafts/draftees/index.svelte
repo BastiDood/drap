4b5a5d13d835b63bb3c2a19b393054e189c58017
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-20 13:01:17 +0800

    feat(drafts/draftees): export loader to drawer component

---
+++ src/lib/features/drafts/draftees/index.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  import * as Drawer from '$lib/components/ui/drawer';
+
+  import Loader from './loader.svelte';
+
+  interface Props {
+    trigger: Snippet;
+    draftId: bigint;
+    queryKey: string;
+    customTextOnEmpty?: string;
+  }
+
+  const { trigger, draftId, queryKey, customTextOnEmpty }: Props = $props();
+</script>
+
+<Drawer.Root>
+  <Drawer.Trigger>
+    {@render trigger()}
+  </Drawer.Trigger>
+  <Drawer.Content class="min-h-screen">
+    <div class="overflow-auto pb-40 px-8">
+      <Loader {draftId} {queryKey} {customTextOnEmpty} />
+    </div>
+  </Drawer.Content>
+</Drawer.Root>
