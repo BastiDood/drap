59739d85555450ed2360a576205fd50cf16e0eec
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2026-03-21 20:44:38 +0800

    feat: add bottom nav bar component

---
+++ src/lib/components/bottom-nav.svelte
@@ -0,0 +1,57 @@
+<script>
+  import HouseIcon from '@lucide/svelte/icons/house';
+  import MenuIcon from '@lucide/svelte/icons/menu';
+
+  import Logo from '$lib/assets/logo-DRAP-icon-colored.svg';
+  import { page } from '$app/state';
+  import { resolve } from '$app/paths';
+  import { TooltipProvider } from '$lib/components/ui/tooltip';
+  // import { useSidebar } from '$lib/components/ui/sidebar/context.svelte';
+
+  let { sidebar } = $props();
+
+  // const sidebar = useSidebar();
+</script>
+
+<TooltipProvider>
+  <nav
+    class="bg-background/90 flex w-full items-center justify-between gap-2 px-10 py-2 backdrop-blur-md"
+  >
+    <div class="flex w-1/3 place-content-center items-center">
+      <button
+        class="active:bg-primary flex w-2/5 place-content-center rounded-lg py-2 dark:active:bg-white"
+        onclick={_ => {
+          sidebar.toggle();
+        }}
+      >
+        <MenuIcon class="size-6 active:text-white dark:active:text-black" />
+      </button>
+      <span class="sr-only">Toggle Sidebar</span>
+    </div>
+
+    <div class="flex w-1/3 place-content-center items-center">
+      <div
+        class="flex w-2/5 place-content-center rounded-lg py-2 {String(
+          page.url.pathname,
+        ).startsWith('/dashboard')
+          ? 'bg-primary dark:bg-white'
+          : 'bg-transparent'}"
+      >
+        <a href={resolve('/dashboard')}>
+          <HouseIcon
+            class="size-6 {String(page.url.pathname).startsWith('/dashboard')
+              ? 'text-white dark:text-black'
+              : 'text-primary'}"
+          />
+        </a>
+      </div>
+      <span class="sr-only">Go to Dashboard</span>
+    </div>
+
+    <button class="flex w-1/3 place-content-center items-center">
+      <a href={resolve('/')}>
+        <img src={Logo} alt="DRAP Logo" class="size-6" />
+      </a>
+    </button>
+  </nav>
+</TooltipProvider>
