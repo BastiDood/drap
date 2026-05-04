2f07f649d6742c99e0e83bbb0ecbfa64f4a0f9d6
Basti Ortiz <ortiz@bastidood.dev>
2026-05-04 22:59:27 +0800

    feat(app): add contributors list
    Closes #235.

--- src/lib/features/contributors/index.svelte
+++ src/lib/features/contributors/index.svelte
@@ -14,14 +14,14 @@
     {
       name: 'Basti Ortiz',
       avatar: 'https://avatars.githubusercontent.com/u/39114273',
-      roles: { '2024': 'Lead Engineer', '2025': 'Lead Engineer', '2026': 'Lead Engineer' },
+      roles: { '2024': 'Lead Engineer', '2025': 'Lead Engineer', '2026': 'Project Head' },
       github: 'BastiDood',
       website: 'https://bastidood.dev/',
     },
     {
       name: 'Jelly Raborar',
       avatar: 'https://avatars.githubusercontent.com/u/98273014',
-      roles: { '2024': 'Designer', '2025': 'Designer', '2026': 'Designer' },
+      roles: { '2024': 'Designer', '2025': 'Designer', '2026': 'Design Team: Head' },
       github: 'Anjellyrika',
     },
     {
@@ -32,11 +32,59 @@
       website: 'https://veeisforvictor.github.io/My-Portfolio/',
     },
     {
+      name: 'Eriene Galinato',
+      avatar: 'https://avatars.githubusercontent.com/u/99855294',
+      roles: { '2026': 'App Team: Head' },
+      github: 'galierie',
+    },
+    {
+      name: 'Harry Quijano',
+      avatar: 'https://avatars.githubusercontent.com/u/87745547',
+      roles: { '2026': 'Infrastructure Team: Head' },
+      github: 'Harry2166',
+    },
+    {
+      name: 'Michael Real',
+      avatar: 'https://avatars.githubusercontent.com/u/60802461',
+      roles: { '2026': 'Infrastructure Team: Engineer' },
+      github: 'mcjefdreal',
+    },
+    {
+      name: 'Ysaac Villamil',
+      avatar: 'https://avatars.githubusercontent.com/u/115415483',
+      roles: { '2026': 'App Team: Engineer' },
+      github: 'LigsQt',
+    },
+    {
       name: 'Ehren Castillo',
       avatar: 'https://avatars.githubusercontent.com/u/59799738',
-      roles: { '2025': 'Engineer', '2026': 'Engineer' },
+      roles: { '2025': 'Engineer', '2026': 'App Team: Engineer' },
       github: 'ehrelevant',
       website: 'https://www.ehrencastillo.tech/',
+    },
+    {
+      name: 'Marcus Pascual',
+      avatar: 'https://avatars.githubusercontent.com/u/149664258',
+      roles: { '2026': 'Design Team: Engineer' },
+      github: 'syncopascual',
+    },
+    {
+      name: 'Ervin Mercado',
+      avatar: 'https://avatars.githubusercontent.com/u/173535366',
+      roles: { '2026': 'App Team: Engineer' },
+      github: 'ervnmrcdo',
+    },
+    {
+      name: 'Benj Lazaro',
+      avatar: 'https://avatars.githubusercontent.com/u/159742520',
+      roles: { '2026': 'Design Team: Engineer' },
+      github: 'Enoux',
+    },
+    {
+      name: 'Jan Albert Quidet',
+      avatar: 'https://avatars.githubusercontent.com/u/55362458',
+      roles: { '2026': 'App Team: Engineer' },
+      github: 'jdquidet',
     },
   ];
 
