c9f959f8d3b266a769db659e399b9a8eab9d9e47
Basti Ortiz <ortiz@bastidood.dev>
2026-05-10 12:56:30 +0800

    fix(env): apply build-only default mocks for crypto variables

---
+++ src/lib/server/env/drap/crypto.ts
@@ -0,0 +1,20 @@
+import assert from 'node:assert/strict';
+import { Buffer } from 'node:buffer';
+
+import { building } from '$app/environment';
+import { env } from '$env/dynamic/private';
+
+// eslint-disable-next-line @typescript-eslint/init-declarations
+let buffer: Buffer<ArrayBuffer>;
+if (building) {
+  // A mock key for builds is fine for now.
+  buffer = Buffer.alloc(32);
+} else {
+  assert(typeof env.DRAP_ENCRYPTION_KEY !== 'undefined');
+  buffer = Buffer.from(env.DRAP_ENCRYPTION_KEY, 'base64url');
+}
+
+export const ENCRYPTION_KEY = await crypto.subtle.importKey('raw', buffer, 'AES-GCM', false, [
+  'encrypt',
+  'decrypt',
+]);
