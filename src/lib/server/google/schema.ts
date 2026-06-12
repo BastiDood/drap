3f5f7fc90164e5413e78142874f78769a5d95e32
Eriene Galinato <edgalinato@up.edu.ph>
2026-06-12 23:49:47 +0800

    feat(google): add `GmailMessageIdResult` schema

--- src/lib/server/google/schema.ts
+++ src/lib/server/google/schema.ts
@@ -22,3 +22,17 @@ export const GmailMessageSendResult = v.object({
   internalDate: v.optional(v.string()),
 });
 export type GmailMessageSendResult = v.InferOutput<typeof GmailMessageSendResult>;
+
+export const GmailMessageIdResult = v.object({
+  ...GmailMessageSendResult.entries,
+  payload: v.object({
+    headers: v.pipe(
+      v.array(v.object({
+        name: v.literal('Message-ID'),
+        value: v.string(),
+      })),
+      v.length(1),
+    ),
+  }),
+});
+export type GmailMessageIdResult = v.InferOutput<typeof GmailMessageIdResult>;
