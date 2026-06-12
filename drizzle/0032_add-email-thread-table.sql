9c4fc35220989ea5d4894d998ab91db4db67c29d
Eriene Galinato <edgalinato@up.edu.ph>
2026-06-12 22:24:11 +0800

    feat(db): generate `emailThread` migration files

---
+++ drizzle/0032_add-email-thread-table.sql
@@ -0,0 +1,16 @@
+CREATE TYPE "public"."inngest_event_type_enum" AS ENUM('round-started', 'round-submitted', 'lottery-intervened', 'draft-concluded', 'draft-finalization', 'user-assigned');--> statement-breakpoint
+CREATE TABLE "email"."email_thread" (
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"id" bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "email"."email_thread_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 CACHE 1),
+	"draft_id" bigint NOT NULL,
+	"event_type" "inngest_event_type_enum" NOT NULL,
+	"round" smallint,
+	"recipient_user_id" "ulid" NOT NULL,
+	"gmail_thread_id" text NOT NULL,
+	"gmail_message_ids" text[] NOT NULL,
+	CONSTRAINT "thread_draft_event_round_recipient_idx" UNIQUE NULLS NOT DISTINCT("draft_id","event_type","round","recipient_user_id"),
+	CONSTRAINT "thread_recipient_idx" UNIQUE("gmail_thread_id","recipient_user_id")
+);
+--> statement-breakpoint
+ALTER TABLE "email"."email_thread" ADD CONSTRAINT "email_thread_draft_id_draft_id_fk" FOREIGN KEY ("draft_id") REFERENCES "drap"."draft"("id") ON DELETE no action ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE "email"."email_thread" ADD CONSTRAINT "email_thread_recipient_user_id_user_id_fk" FOREIGN KEY ("recipient_user_id") REFERENCES "drap"."user"("id") ON DELETE cascade ON UPDATE cascade;
