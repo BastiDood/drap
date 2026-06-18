ab4fa8da56c264f646fad26f102de2f0370b7a61
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-06-18 14:31:38 +0800

    refactor: use user listbox item component for student selection screen

--- src/lib/features/faculty/previous-picks/index.svelte
+++ src/lib/features/faculty/previous-picks/index.svelte
@@ -3,8 +3,8 @@
 
   import * as Card from '$lib/components/ui/card';
   import * as Tabs from '$lib/components/ui/tabs';
-  import DraftAvatar from '$lib/components/draft-avatar.svelte';
   import Empty from '$lib/components/empty.svelte';
+  import UserlistItem from '$lib/components/userlist-item.svelte';
   import type { schema } from '$lib/server/database/drizzle';
 
   interface Researcher extends Pick<
@@ -50,29 +50,18 @@
             <ul class="space-y-1">
               {#each students as { email, givenName, familyName, avatarObjectKey, studentNumber } (email)}
                 <li>
-                  <a
-                    href="mailto:{email}"
-                    class="flex items-center gap-3 rounded-md bg-muted p-2 transition-colors duration-150 hover:bg-muted/80"
-                  >
-                    {#if avatarObjectKey === null}
-                      <DraftAvatar class="size-10" />
-                    {:else}
-                      <DraftAvatar
-                        avatar={{
-                          objectKey: avatarObjectKey,
-                          alt: `${givenName} ${familyName}`,
-                        }}
-                        class="size-10"
-                      />
-                    {/if}
-                    <div class="flex grow flex-col">
-                      <strong><span class="uppercase">{familyName}</span>, {givenName}</strong>
-                      {#if studentNumber !== null}
-                        <span class="text-sm opacity-50">{studentNumber}</span>
-                      {/if}
-                      <span class="text-xs opacity-50">{email}</span>
-                    </div>
-                  </a>
+                  <UserlistItem
+                    {email}
+                    {givenName}
+                    {familyName}
+                    {studentNumber}
+                    avatar={{
+                      variant: 'draft',
+                      objectKey: avatarObjectKey,
+                      alt: `${givenName} ${familyName}`,
+                    }}
+                    class="bg-muted transition-colors duration-150 hover:bg-muted/80"
+                  />
                 </li>
               {/each}
             </ul>
